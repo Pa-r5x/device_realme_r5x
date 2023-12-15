1f712197b462b9d6d4fe9341fbf3797f695eae97
Georg Veichtlbauer <georg@vware.at>
2023-12-15 13:46:31 +0100

    r5x: Drop console from kernel cmdline
    Change-Id: Icb5765d2f97111abdaafc894bbd36d9b78c82f4e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,7 +92,6 @@ TARGET_KERNEL_CONFIG := biofrost_defconfig
 BOARD_KERNEL_CMDLINE := \
       console=ttyMSM0,115200n8 \
       androidboot.hardware=qcom \
-      androidboot.console=ttyMSM0 \
       lpm_levels.sleep_disabled=1 \
       msm_rtb.filter=0x237 \
       service_locator.enable=1 \
