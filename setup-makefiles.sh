b2a5f5b0152cbf75dea4e5999a1628e06c4e994b
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-09-02 21:24:13 +0200

    r5x: Append `_vendor` to conflicting vendor libs
    Change-Id: I74306c87e12079204223caccc0c77eacd4e01ec0
    Signed-off-by: Amritorock <amritokarmokar5i@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -45,6 +45,50 @@ function vendor_imports() {
 EOF
 }
 
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "vendor" ]; then
+        return 1
+    fi
+
+    case "$1" in
+            com.qualcomm.qti.dpm.api@1.0 | \
+            com.qualcomm.qti.imscmservice* | \
+            com.qualcomm.qti.uceservice* | \
+            libmmosal | \
+            vendor.qti.data.* | \
+            vendor.qti.hardware.data.* | \
+            vendor.qti.hardware.fm@1.0 | \
+            vendor.qti.hardware.mwqemadapter@1.0 | \
+            vendor.qti.hardware.radio.am@1.0 | \
+            vendor.qti.hardware.radio.ims@* | \
+            vendor.qti.hardware.radio.internal.deviceinfo@1.0 | \
+            vendor.qti.hardware.radio.lpa* | \
+            vendor.qti.hardware.radio.qcrilhook@1.0 | \
+            vendor.qti.hardware.radio.qtiradio* | \
+            vendor.qti.hardware.radio.uim* | \
+            vendor.qti.hardware.radio.uim_remote_client* | \
+            vendor.qti.hardware.radio.uim_remote_server@1.0 | \
+            vendor.qti.hardware.wifidisplaysession@1.0 | \
+            vendor.qti.ims.callcapability@1.0 | \
+            vendor.qti.ims.callinfo@1.0 | \
+            vendor.qti.ims.factory* | \
+            vendor.qti.ims.rcsconfig* | \
+            vendor.qti.imsrtpservice@3.0 | \
+            vendor.qti.latency*)
+            echo "${1}_vendor"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_vendor_variants "$@"
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
