de22f552d596ea6d42b25f31b72bb8e8d7432025
Amrito Karmokar <amritokarmokar5i@gmail.com>
2024-10-21 15:29:30 +0600

    r5x: Inherit wfd component
    Signed-off-by: Amritorock <amritokarmokar5i@gmail.com>

--- device.mk
+++ device.mk
@@ -233,8 +233,7 @@ PRODUCT_COPY_FILES += \
 # Minijail
 PRODUCT_PACKAGES += \
     libminijail \
-    libminijail.vendor \
-    libavservices_minijail
+    libminijail.vendor
 
 # Netutils
 PRODUCT_PACKAGES += \
@@ -313,6 +312,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     perf \
     usb \
     vibrator \
+    wfd \
     wlan
 
 # RIL
@@ -370,11 +370,6 @@ PRODUCT_COPY_FILES += \
 # Wifi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
-
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libnl \
-    vendor.display.config@2.0
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/realme/r5x/r5x-vendor.mk)
