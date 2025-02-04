9f1959bd72234411584139678b7d96196e80de54
mikeNG <mikeNG@users.noreply.github.com>
2025-02-04 21:31:27 +0000

    r5x: fix legacy libprotobuf-cpp-full ELF check
    Signed-off-by: Amrito Karmokar <amritokarmokar5i@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -75,6 +75,9 @@ function lib_to_package_fixup_vendor_variants() {
             libgrallocutils | \
             libwpa_client)
             ;;
+            libprotobuf-cpp-full)
+            echo "libprotobuf-cpp-full-vendorcompat"
+            ;;
         *)
             return 1
             ;;
