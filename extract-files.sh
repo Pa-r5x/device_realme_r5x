f01ade0455ed893ac008b1ce1744f56dcbf645f0
AmritoRock <amritokarmokar5i@gmail.com>
2025-02-22 20:30:32 +0600

    r5x: Inherit Charging component
    Signed-off-by: AmritoRock <amritokarmokar5i@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -55,10 +55,6 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        vendor/bin/hw/android.hardware.health@2.0-service.oppo)
-            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
-            "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
-            ;;
         vendor/lib64/libwvhidl.so)
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
             "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
