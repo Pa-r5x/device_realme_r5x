8fb401bc6952520469e570c1bbe0df1cd58d60d3
Amrito Karmokar <amritokarmokar5i@gmail.com>
2025-04-10 08:01:34 +0000

    r5x: Drop in tree GPS hal
    Signed-off-by: Amrito Karmokar <amritokarmokar5i@gmail.com>

--- device.mk
+++ device.mk
@@ -170,7 +170,7 @@ PRODUCT_PACKAGES += \
     gnss@2.0-xtwifi-inet-agent.policy
 
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/gps/etc/,$(TARGET_COPY_OUT_VENDOR)/etc)
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/gps/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
