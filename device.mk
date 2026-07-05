7425d52b77235468a2cf015bc8d20adee723346d
dodyirawan85 <dev.irawans@gmail.com>
2026-07-05 18:34:21 +0000

    r5x: Switch to OSS egistec hals
    - nuke useless sepolicy lable
    
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>
    Signed-off-by: AmritoRock <amritokarmokar5i@gmail.com>

--- device.mk
+++ device.mk
@@ -8,7 +8,8 @@ OVERRIDE_PRODUCT_COMPRESSED_APEX := false
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/egistec/fingerprint
 
 # ANT+
 PRODUCT_PACKAGES += \
@@ -133,7 +134,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.r5x
+    android.hardware.biometrics.fingerprint@2.1-service.r5x \
+    android.hardware.biometrics.fingerprint@2.3-service.rbs
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
